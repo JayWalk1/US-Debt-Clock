6c4715923d7970b04a79d857cd8c064b5b54e34f
JayWalk1 <107002541+JayWalk1@users.noreply.github.com>
2024-07-15 15:21:00 +0100

    Update debtClock.js

--- debtClock.js
+++ debtClock.js
@@ -1,6 +1,6 @@
 async function fetchDebtData() {
     console.log("Fetching data...");
-    const response = await fetch('https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/debt_to_penny?sort=-record_date');
+    const response = await fetch('https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/debt_to_penny?sort=-record_date&page[size]=1');
     const data = await response.json();
     console.log("Data fetched:", data);
     // Fetch the first record which is the most recent due to sorting by record_date in descending order
